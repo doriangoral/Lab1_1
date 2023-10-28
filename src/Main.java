5e92936b488b289f485b116abed2990501624763
Dorian <dorian.goral@icloud.com>
2023-10-28 09:27:27 +0200

    Zadanie 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        String name = "Dorian";
+        int age = 22;
+        String major = "Informatyka";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Major: " + major);
     }
 }
